074569a27de1055c4c7900b9ecbd63845e0bdefe
Tigran <abvgdesh3@gmail.com>
2022-10-05 20:45:15 +0300

    Enemy attack and spawning

---
+++ scenes/enemies/Spawner.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var enemyScene = load("res://scenes/enemies/simple_enemy/SimpleEnemy.tscn")
+var path = []
+
+func _ready() -> void:
+	yield(owner, "ready")
+
+func spawn_enemy() -> void:
+	var enemy = enemyScene.instance()
+	enemy.initialize(global_position, owner.get_path_to_target(global_position))
+	owner.add_child(enemy)
