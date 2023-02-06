f3f87e3ea35ece92439f9e7284cb832b9d4531b9
Tigran Shaginyan <abvgdesh3@gmail.com>
2023-02-06 17:06:10 +0300

    DeathMenu

--- scenes/menu/death_menu/DeathMenu.gd
+++ scenes/menu/death_menu/DeathMenu.gd
@@ -2,31 +2,24 @@ extends Control
 
 export (String, FILE, "*.tscn,*.scn") var menuScene = "res://scenes/menu/main_menu/MainMenu.tscn"
 
-onready var options = $Options
-
 var is_paused = false setget set_is_paused
-onready var player = get_parent().get_parent()
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("pause") && !player.is_dead:
-		self.is_paused = !is_paused
+func _ready():
+	var player = get_parent().get_parent()
+	if (player != null):
+		player.connect("playerDead", self, "show_death_screen")
+
+func show_death_screen():
+	self.is_paused = !is_paused
 
 func set_is_paused(value):
-	options.hide()
 	is_paused = value
 	get_tree().paused = is_paused
 	visible = is_paused
 
-func _on_Resume_pressed() -> void:
-	self.is_paused = false
-
-func _on_Settings_pressed() -> void:
-	options.show()
-
 func _on_Exit_pressed() -> void:
 	self.is_paused = false
 	get_tree().change_scene(menuScene)
-
 
 func _on_Restart_pressed() -> void:
 	self.is_paused = false
